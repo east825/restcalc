c2a77bd8705c444c2ebd8d2e47a7cfc6c5bc085a
Mikhail Golubev <qsolo825@gmail.com>
2013-03-10 02:31:14 +0400

    Extracted all expression evaluation logic to CalculatorResource
    I've tried to avoid modifying of xjc-generated classes in any way.
    (Cause if there were thousands of them it'd be rather tedious)
    So I moved all the logic responsible for calculating results to the
    resource itself

--- src/main/java/restcalc/CalculatorResource.java
+++ src/main/java/restcalc/CalculatorResource.java
@@ -1,8 +1,7 @@
 package restcalc;
 
 import org.xml.sax.SAXException;
-import restcalc.expr.Expression;
-import restcalc.expr.NumberExpression;
+import restcalc.expr.*;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -12,6 +11,7 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import java.io.StringReader;
 import java.net.URL;
+import java.util.List;
 
 @Path("/calc")
 public class CalculatorResource {
@@ -48,18 +48,59 @@ public class CalculatorResource {
         unmarshaller.setSchema(schema);
     }
 
+    private ObjectFactory objectFactory = new ObjectFactory();
+
+    private <T> double evaluate(T e) {
+        if (e instanceof NumberExpression) {
+            return ((NumberExpression) e).getValue();
+        } else if (e instanceof NegExpression) {
+            NegExpression ne = (NegExpression) e;
+            if (ne.getNum() != null)
+                return -evaluate(ne.getNum());
+            if (ne.getSum() != null)
+                return -evaluate(ne.getNum());
+            if (ne.getSub() != null)
+                return -evaluate(ne.getNum());
+            if (ne.getDiv() != null)
+                return -evaluate(ne.getNum());
+            if (ne.getMult() != null)
+                return -evaluate(ne.getNum());
+            if (ne.getNeg() != null)
+                return -evaluate((ne.getNeg()));
+            throw new IllegalArgumentException("Unknown subexpression of NegExpression");
+        } else if (e instanceof SumExpression) {
+            List<Object> es = ((SumExpression) e).getSumOrSubOrMult();
+            return evaluate(es.get(0)) + evaluate(es.get(1));
+        } else if (e instanceof SubExpression){
+            List<Object> es = ((SubExpression) e).getSumOrSubOrMult();
+            return evaluate(es.get(0)) - evaluate(es.get(1));
+        } else if (e instanceof MultExpression){
+            List<Object> es = ((MultExpression) e).getSumOrSubOrMult();
+            return evaluate(es.get(0)) * evaluate(es.get(1));
+        } else if (e instanceof DivExpression){
+            List<Object> es = ((DivExpression) e).getSumOrSubOrMult();
+            return evaluate(es.get(0)) / evaluate(es.get(1));
+        } else {
+            throw new IllegalArgumentException("Unknown expression type");
+        }
+    }
+
     @POST
     @Consumes(MediaType.APPLICATION_XML)
     @Produces(MediaType.APPLICATION_XML)
-    public NumberExpression calcExpression(String xmlSource) {
-        Expression expr;
+    public JAXBElement<NumberExpression> calcExpression(String xmlSource) {
+        JAXBElement<?> expr;
         try {
-            expr = (Expression) JAXBIntrospector.getValue(unmarshaller.unmarshal(new StringReader(xmlSource)));
+            expr = (JAXBElement<?>) unmarshaller.unmarshal(new StringReader(xmlSource));
         } catch (JAXBException e) {
             throw new WebApplicationException(400);
         }
-        NumberExpression res = new NumberExpression();
-        res.setValue(expr.evaluate());
-        return res;
+//        System.out.println(expr.getName().getLocalPart());
+//        System.out.println(((SumExpression)expr.getValue()).getSumOrSubOrMult().get(0));
+//        NumberExpression res = new NumberExpression();
+//        res.setValue(evaluate(expr));
+        NumberExpression result = objectFactory.createNumberExpression();
+        result.setValue(evaluate(expr.getValue()));
+        return objectFactory.createNum(result);
     }
 }
